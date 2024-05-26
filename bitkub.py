31309e632dda61e55d4c48ad10f7f42d8db348f6
HFTlab <arbitragecryptobotnet@gmail.com>
2024-05-26 16:48:19 +0200

    updated autoconvertation of USDT

--- bitkub.py
+++ bitkub.py
@@ -145,10 +145,9 @@ class BitKubClient:
             # market = self.markets[self.markets_list[random.randint(0, len(self.markets_list) - 1)]]
             market = 'THB_USDT'
             # self.get_server_time()
-            if size_usdt := self.balance.get('USDT') >= 1:
+            if self.balance.get('USDT') >= 1:
                 price = self.get_orderbook(market)['asks'][0][0] * 1.001
-                print(size_usdt)
-                await self.create_fast_order(price, float(size_usdt), "sell", market, "keep-alive")
+                await self.create_fast_order(price, self.balance.get('USDT'), "sell", market, "keep-alive")
             else:
                 price = self.get_orderbook(market)['bids'][0][0] * .9
                 await self.create_fast_order(price, 1, "buy", market, "keep-alive")
